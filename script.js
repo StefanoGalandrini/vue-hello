d40845bca1d6f02e63035868257b62514bdb51c0
StefanoGalandrini <stefe68@gmail.com>
2023-05-05 14:39:23 +0200

    Add link to Vue variables for text and images

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const app = Vue.createApp({
 	data() {
 		return {
 			title: "Castelli famosi d'Irlanda",
+			img1src: "img/copertina.jpg",
+			img2src: "img/castello.jpg",
+			textCover: "Copertina del disco",
+			textCastle: "il Moydrum Castle, fra Dublino e Galway",
 		};
 	},
 });
